2b6dbd6a986f782854885b9f930b2e940c87c5f8
avicennamumtaza <avislamongan@gmail.com>
2024-04-03 23:28:37 +0700

    fix: faker option for umkm seeder

--- database/seeders/UmkmSeeder.php
+++ database/seeders/UmkmSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class UmkmSeeder extends Seeder
 {
@@ -15,7 +16,7 @@ class UmkmSeeder extends Seeder
     {
         $data = [
             [
-                'id_umkm' => 1,
+                'id_umkm' => 2,
                 'no_rw' => 6,
                 'nik_pemilik' => 3573011233455985,
                 'nama_umkm' => 'Risol Dea',
@@ -25,5 +26,16 @@ class UmkmSeeder extends Seeder
             ]
         ];
         DB::table('umkm')->insert($data);
+        // $faker = Faker::create();
+
+        // for ($i = 1; $i < 10; $i++) {
+        //     DB::table('umkm')->insert([
+        //         'id_umkm' => $i,
+        //         'nik_pemilik' => $faker->randomNumber($nbDigits = 16, $strict = true),
+        //         'nama_umkm' => $faker->name(),
+        //         'foto_umkm' => $faker->text($maxNbChars = 50),
+        //         'desc_umkm' => $faker->text($maxNbChars = 50),
+        //     ]);
+        // }
     }
 }
