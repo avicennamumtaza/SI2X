8d59cf58215a20b863c0d469698f8cc07e5ab38b
avicennamumtaza <avislamongan@gmail.com>
2024-04-03 23:23:45 +0700

    fix: fix submit umkm method

--- app/Http/Controllers/UmkmController.php
+++ app/Http/Controllers/UmkmController.php
@@ -9,17 +9,27 @@ class UmkmController extends Controller
 {
     public function submitUmkm(Request $request)
     {
+        // dd($request->all());
         // Validasi input
         $request->validate([
-            'nama_umkm' => 'required|string|max:255',
-            'deskripsi' => 'required|string',
+            'nik_pemilik' => 'required|string|max:17',
+            'nama_umkm' => 'required|string|max:50',
+            'foto_umkm' => 'required|string|max:50',
+            'desc_umkm' => 'required|string',
             // Tambahkan validasi untuk input lainnya jika diperlukan
         ]);
 
         // Simpan data UMKM ke dalam database
         $umkm = new Umkm();
-        $umkm->nama = $request->nama_umkm;
-        $umkm->deskripsi = $request->deskripsi;
+        $umkm->nama_umkm = $request->nama_umkm;
+        $umkm->nik_pemilik = $request->nik_pemilik;
+        $umkm->foto_umkm = $request->foto_umkm;
+        $umkm->desc_umkm = $request->desc_umkm;
+
+        // Default value
+        $umkm->no_rw = 6;
+        $umkm->status_umkm = 'new';
+
         // Tambahkan penyimpanan untuk input lainnya jika diperlukan
         $umkm->save();
 
