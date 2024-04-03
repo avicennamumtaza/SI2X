48127666b239c00f7a243327dc8b6a1223de33ff
avicennamumtaza <avislamongan@gmail.com>
2024-04-04 06:38:42 +0700

    feat: add umkm seeder using faker

--- database/seeders/UmkmSeeder.php
+++ database/seeders/UmkmSeeder.php
@@ -14,28 +14,33 @@ class UmkmSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
-            [
-                'id_umkm' => 2,
-                'no_rw' => 6,
-                'nik_pemilik' => 3573011233455985,
-                'nama_umkm' => 'Risol Dea',
-                'foto_umkm' => 'disini/yaaaa',
-                'desc_umkm' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                'status_umkm' => 'Diterima'
-            ]
-        ];
-        DB::table('umkm')->insert($data);
-        // $faker = Faker::create();
+        // $data = [
+        //     [
+        //         'id_umkm' => 2,
+        //         'no_rw' => 6,
+        //         'nik_pemilik' => 3573011233455985,
+        //         'nama_umkm' => 'Risol Dea',
+        //         'foto_umkm' => 'disini/yaaaa',
+        //         'desc_umkm' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        //         'status_umkm' => 'Diterima'
+        //     ]
+        // ];
+        // DB::table('umkm')->insert($data);
+        
+        $faker = Faker::create();
 
-        // for ($i = 1; $i < 10; $i++) {
-        //     DB::table('umkm')->insert([
-        //         'id_umkm' => $i,
-        //         'nik_pemilik' => $faker->randomNumber($nbDigits = 16, $strict = true),
-        //         'nama_umkm' => $faker->name(),
-        //         'foto_umkm' => $faker->text($maxNbChars = 50),
-        //         'desc_umkm' => $faker->text($maxNbChars = 50),
-        //     ]);
-        // }
+        foreach (range(1, 20) as $index) {
+            DB::table('umkm')->insert([
+                'no_rw' => $faker->numberBetween(1, 100),
+                'nik_pemilik' => $faker->numerify('################'),
+                'nama_umkm' => $faker->company,
+                'wa_umkm' => $faker->numerify('08##########'),
+                'foto_umkm' => $faker->imageUrl(),
+                'desc_umkm' => $faker->paragraph,
+                'status_umkm' => $faker->randomElement(['Diterima', 'Ditolak', 'Baru']),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
