8b7deccbf72a190a713a4baaec876e6d909a0e3f
Ana Bellatus <57426532+anabellatus@users.noreply.github.com>
2024-04-04 17:31:55 +0700

    fix: user migration

--- database/migrations/2024_04_03_154342_users.php
+++ database/migrations/2024_04_03_154342_users.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id('id_user');
             $table->string('nik',16);
             $table->string('username',20);
