2548d9fc8878a220279c9d948996e3b474109800
avicennamumtaza <avislamongan@gmail.com>
2024-04-06 12:50:08 +0700

    feat: add pengumuman dataTable

---
+++ app/DataTables/PengumumanDataTable.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Pengumuman;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class PengumumanDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            // ->addColumn('action', 'pengumuman.action')
+            ->setRowId('id')
+            ->addcolumn('action', function($row){
+                $deleteUrl = route('pengumuman.destroy', $row->id_pengumuman);
+                $action = '
+                <form action="' . $deleteUrl . '" method="post">
+                    ' . csrf_field() . '
+                    ' . method_field('DELETE') . '
+                    <button type="submit" class="delete btn-delete btn-sm">Delete</button>
+                </form>
+                ';
+                // return $action;
+                // $action = '<a href="kategori/edit/'.$row->kategori_id.'" class="edit btn-primary  btn-sm">Edit</a>';
+                // $action .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                return $action;
+            });
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(Pengumuman $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('pengumuman-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->orderBy(0, 'asc') // Set default order by column 0 (id_pengumuman)
+                    ->parameters([
+                        'dom' => 'Bfrtip', // Menambahkan tombol
+                        'buttons' => ['excel', 'csv', 'pdf', 'print', 'reset', 'reload'], // Menambahkan tombol ekspor dan lainnya
+                        'order' => [], // Mengaktifkan order by untuk setiap kolom
+                    ])
+                    ->selectStyleSingle();
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            // Column::computed('action')
+            //       ->exportable(false)
+            //       ->printable(false)
+            //       ->width(60)
+            //       ->addClass('text-center'),
+            Column::make('id_pengumuman'),
+            Column::make('judul'),
+            Column::make('deskripsi'),
+            Column::make('tanggal_pengumuman'),
+            Column::make('action'),
+            // Column::make('created_at'),
+            // Column::make('updated_at'),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'Pengumuman_' . date('YmdHis');
+    }
+}
