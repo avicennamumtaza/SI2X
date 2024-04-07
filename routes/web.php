ca690b29155bf483fa222619d39ef1e551d22dae
AfrizalDS <angelicacoustarika@gmail.com>
2024-04-07 15:34:57 +0700

    fix :  umkm routes

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,9 @@ Route::get('/umkm', [UmkmController::class, 'index'])->name('umkm.global');
 Route::get('/umkmm', [UmkmController::class, 'list'])->name('umkm.manage');
 // Route::get('/umkm/{id}/edit', [UmkmController::class, 'edit'])->name('umkm.edit');
 // Route::put('/umkm/{id}', [UmkmController::class, 'update'])->name('umkm.update');
-Route::delete('/umkm/{id}', [UmkmController::class, 'destroy'])->name('umkm.destroy');
+Route::delete('/umkmm/{id}', [UmkmController::class, 'destroy'])->name('umkm.destroy');
+Route::get('/umkmm/{id}/edit', [UmkmController::class, 'edit'])->name('umkm.edit');
+
 
 // global pengumuman
 Route::get('/pengumuman', [PengumumanController::class, 'index'])->name('pengumuman.global');
