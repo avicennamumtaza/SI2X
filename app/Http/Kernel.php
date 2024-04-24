6ad853edfc03e1e9afff0d08794c3c25a7d8b9b8
avicennamumtaza <avislamongan@gmail.com>
2024-04-24 23:44:03 +0700

    feat: add middleware for Rt Rw authorization

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'isRt' => \App\Http\Middleware\IsRt::class,
+        'isRw' => \App\Http\Middleware\IsRw::class,
     ];
 }
---
+++ app/Http/Middleware/IsRt.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsRt
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Memeriksa peran pengguna
+        if (!auth()->check() || auth()->guest() || auth()->user()->role !== 'Rt') {
+            // Jika peran pengguna bukan 'rt', munculkan error 403
+            abort(403);
+        }
+        return $next($request);
+    }
+}
