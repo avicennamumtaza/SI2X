b247966e8ce5b11371e8032f9a597573d5a79a53
Ana Bellatus <57426532+anabellatus@users.noreply.github.com>
2024-05-11 13:43:33 +0700

    feat: forgot password

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
 class Users extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, CanResetPassword;
 
     /**
      * The attributes that are mass assignable.
