702330b8365fbd1a0fe104832d2286ceed5e7348
DeaPutri17 <putrinastitidea@gmail.com>
2024-05-18 15:37:16 +0700

    fix: differentiating between a user and a profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -166,56 +166,6 @@ class UsersController extends Controller
         return redirect()->back()->with('success', 'Data berhasil dihapus!');
     }
 
-    public function profil() {
-        $user = auth()->user();
-        return view('auth.rw.profil', compact('user'));
-    }
     
-    public function updateProfil(Request $request, Users $users)
-    {
-
-        // Validasi input
-        $validated = $request->validate([
-            'username' => 'required|string|max:20',
-            'nik' => 'required|string|min:15|max:17',
-            'email' => 'required|string|email|max:50',
-            'foto_profil' => 'nullable|mimes:png,jpg,jpeg',
-            'password' => 'nullable|string|min:6|confirmed',
-        ]);     
-
-        // Update user
-        try {
-            $users->update([
-                'username' => $validated['username'],
-                'foto_profil' => $validated['foto_profil'],
-                'email' => $validated['email'],
-            ]);
-
-            // Hanya update password jika field tersebut diisi
-            if (!empty($validated['password'])) {
-                $users->password = Hash::make($validated['password']);
-                $users->save();
-            }
-
-            return redirect()->back()->with('success', 'User berhasil diupdate!');
-        } catch (\Exception $e) {
-            return redirect()->back()->with('error', 'Update gagal: ' . $e->getMessage());
-        }
-    }
-    
-    public function changePassword(Request $request, Users $user)
-    {
-        // Validasi request
-        $request->validate([
-            'new_password' => 'required|min:8|confirmed', // Konfirmasi password baru
-        ]);
-
-        // Setel password baru
-        $user->password = Hash::make($request->new_password);
-        $user->save();
-
-        // Redirect kembali ke halaman profil dengan pesan sukses
-        return redirect()->route('profil')->with('success', 'Password berhasil diubah.');
-    }
 
 }
