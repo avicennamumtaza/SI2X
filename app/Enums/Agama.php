572d7211023cfccb35425feb80006fc8a38f75d0
AfrizalDS <angelicacoustarika@gmail.com>
2024-05-18 17:59:27 +0700

    Feat : Add file enumeration for Agama

---
+++ app/Enums/Agama.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum Agama: string
+{
+    case ISLAM = "Islam";
+    case KRISTEN = "Kristen";
+    case KATOLIK = "Katolik";
+    case HINDU = "Hindu";
+    case BUDHA = "Budha";
+    case KONGHUCU = "Konghucu";
+}
