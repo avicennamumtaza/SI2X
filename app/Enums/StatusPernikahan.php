f65396f4b41443e75751b35534bc2913b49fdaf7
Ana Bellatus <57426532+anabellatus@users.noreply.github.com>
2024-05-19 17:27:41 +0700

    fix: correct typo in StatusPernikahan enum value

--- app/Enums/StatusPernikahan.php
+++ app/Enums/StatusPernikahan.php
@@ -6,7 +6,7 @@ enum StatusPernikahan: string
 {
     case KAWIN = "Kawin";
     case BELUM_KAWIN = "Belum Kawin";
-    case CERAU_HIDUP = "Cerai Hidup";
+    case CERAI_HIDUP = "Cerai Hidup";
     case CERAI_MATI = "Cerai Mati";
 
 }
