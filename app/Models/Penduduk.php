a071b5e7815b6bd5d48b0d47d3ecffda5e016eef
Ana Bellatus <57426532+anabellatus@users.noreply.github.com>
2024-05-19 12:54:32 +0700

    fix: cast pekerjaan enumeration to penduduk model

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Enums\Agama;
 use App\Enums\GolDar;
 use App\Enums\JenisKelamin;
+use App\Enums\Pekerjaan;
 use App\Enums\Pendidikan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -39,6 +40,7 @@ class Penduduk extends Model
         'jenis_kelamin' => JenisKelamin::class,
         'agama' => Agama::class,
         'pendidikan' => Pendidikan::class,
+        'pekerjaan' => Pekerjaan::class,
     ];
 
     // public function keluarga(): BelongsTo
