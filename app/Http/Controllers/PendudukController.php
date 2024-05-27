de8210e0ceb9ad025f2d51f20d952b68e74fb253
avicennamumtaza <avislamongan@gmail.com>
2024-05-28 01:16:52 +0700

    fix: add import validation of entire column

--- app/Http/Controllers/PendudukController.php
+++ app/Http/Controllers/PendudukController.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 
 class PendudukController extends Controller
 {
@@ -30,7 +31,7 @@ class PendudukController extends Controller
         $pekerjaans = Pekerjaan::cases();
         $pendidikans = Pendidikan::cases();
 
-        return $dataTable->render('auth.rw.penduduk', compact('no_rts', 'nkks', 'goldar', 'pekerjaans', 'pendidikans', 'sp','jk','agamas'));
+        return $dataTable->render('auth.rw.penduduk', compact('no_rts', 'nkks', 'goldar', 'pekerjaans', 'pendidikans', 'sp', 'jk', 'agamas'));
         // return $dataTable->render('auth.rw.penduduk', ['goldar' => GolDar::cases()]);
     }
     public function store(Request $request)
@@ -87,7 +88,7 @@ class PendudukController extends Controller
     {
 
         $request->validate([
-            'nik' => 'required|string|min:15|max:17|unique:penduduk,nik,' . $penduduk->nik. ',nik', // gbisa edit primary key
+            'nik' => 'required|string|min:15|max:17|unique:penduduk,nik,' . $penduduk->nik . ',nik', // gbisa edit primary key
             'nkk' => 'required|string|min:15|max:17',
             'no_rt' => 'required|string|max:2',
             'nama' => 'required|string|max:49',
@@ -119,85 +120,196 @@ class PendudukController extends Controller
 
     //impor csv
     public function import(Request $request)
-{
-    $file = $request->file('file');
-    $fileContents = file($file->getPathname());
+    {
+        $file = $request->file('file');
 
-    foreach ($fileContents as $line) {
-        $data = str_getcsv($line);
-        Penduduk::create([
-            'nik' => $data[0] ?? null,
-            'nkk' => $data[1] ?? null,
-            'no_rt' => $data[2] ?? null,
-            'nama' => $data[3] ?? null,
-            'tempat_lahir' => $data[4] ?? null,
-            'tanggal_lahir' => $data[5] ?? null,
-            'alamat' => $data[6] ?? null,
-            'jenis_kelamin' => $data[7] ?? null,
-            'agama' => $data[8] ?? null,
-            'pendidikan' => $data[9] ?? null,
-            'pekerjaan' => $data[10] ?? null,
-            'golongan_darah' => $data[11] ?? null,
-            'status_pernikahan' => $data[12] ?? null,
-            'status_pendatang' => $data[13] ?? null
-            // Tambahkan kolom lagi jika diperlukan
-        ]);
-        if (count($data) !== 14) {
-            return redirect()->back()->with('error', 'CSV tidak valid.');
+        if (!$file) {
+            Alert::error('Error', 'File tidak ditemukan.');
+            return redirect()->back();
         }
+
+        $fileContents = file($file->getPathname());
+
+        foreach ($fileContents as $key => $line) {
+            try {
+                $data = str_getcsv($line);
+
+                if (count($data) !== 14) {
+                    throw new \Exception('CSV tidak valid. Setiap baris harus memiliki 14 kolom.');
+                }
+
+                // Validasi input per baris
+                $validated = Validator::make([
+                    'nik' => $data[0],
+                    'nkk' => $data[1],
+                    'no_rt' => $data[2],
+                    'nama' => $data[3],
+                    'tempat_lahir' => $data[4],
+                    'tanggal_lahir' => $data[5],
+                    'alamat' => $data[6],
+                    'jenis_kelamin' => $data[7],
+                    'agama' => $data[8],
+                    'pendidikan' => $data[9],
+                    'pekerjaan' => $data[10],
+                    'golongan_darah' => $data[11],
+                    'status_pernikahan' => $data[12],
+                    'status_pendatang' => $data[13]
+                ], [
+                    'nik' => 'required|string|min:15|max:17|unique:penduduk,nik',
+                    'nkk' => 'required|string|min:15|max:17',
+                    'no_rt' => 'required|string|max:2',
+                    'nama' => 'required|string|max:49',
+                    'tempat_lahir' => 'required|string|min:2|max:49',
+                    'tanggal_lahir' => 'required|date',
+                    'alamat' => 'required|string|min:5',
+                    'jenis_kelamin' => [Rule::enum(JenisKelamin::class)],
+                    'agama' => [Rule::enum(Agama::class)],
+                    'pendidikan' => [Rule::enum(Pendidikan::class)],
+                    'pekerjaan' => [Rule::enum(Pekerjaan::class)],
+                    'golongan_darah' => [Rule::enum(GolDar::class)],
+                    'status_pernikahan' => [Rule::enum(StatusPernikahan::class)],
+                    'status_pendatang' => 'required',
+                ])->validate();
+
+                // Jika validasi berhasil, buat entri baru di database
+                Penduduk::create($validated);
+            } catch (\Exception $e) {
+                // Alert::error('Error', '. Pesan: ' . $e->getMessage());
+                Alert::error('Kesalahan pada baris ' . $key+1 , '' . $e->getMessage());
+                // Alert::error('Kesalahan pada baris ' . $key+1 , '' . function () {
+                //     foreach ($e->getMessage() as $singleE) {
+                //         $singleE;
+                //     }
+                // });
+                return redirect()->back();
+            }
+        }
+
+        return redirect()->back()->with('success', 'CSV berhasil diimpor.');
     }
 
-    return redirect()->back()->with('success', 'CSV berhasil diimpor.');
-}
-public function export()
-{
-    $penduduk = Penduduk::all();
-    $filename = 'penduduk.csv';
-    $handle = fopen($filename, 'w+');
-    fputcsv($handle, [
-        'nik',
-        'nkk',
-        'no_rt',
-        'nama',
-        'tempat_lahir',
-        'tanggal_lahir',
-        'alamat',
-        'jenis_kelamin',
-        'agama',
-        'pendidikan',
-        'pekerjaan',
-        'golongan_darah',
-        'status_pernikahan',
-        'status_pendatang'
-        // Tambahkan kolom lagi jika diperlukan
-    ]);
-
-    foreach ($penduduk as $row) {
+    public function export()
+    {
+        $penduduk = Penduduk::all();
+        $filename = 'penduduk.csv';
+        $handle = fopen($filename, 'w+');
         fputcsv($handle, [
-            $row->nik,
-            $row->nkk,
-            $row->no_rt,
-            $row->nama,
-            $row->tempat_lahir,
-            $row->tanggal_lahir,
-            $row->alamat,
-            $row->jenis_kelamin->getDescription(),
-            $row->agama->getDescription(),
-            $row->pendidikan->getDescription(),
-            $row->pekerjaan->getDescription(),
-            $row->golongan_darah->getDescription(),
-            $row->status_pernikahan->getDescription(),
-            $row->status_pendatang
+            'nik',
+            'nkk',
+            'no_rt',
+            'nama',
+            'tempat_lahir',
+            'tanggal_lahir',
+            'alamat',
+            'jenis_kelamin',
+            'agama',
+            'pendidikan',
+            'pekerjaan',
+            'golongan_darah',
+            'status_pernikahan',
+            'status_pendatang'
             // Tambahkan kolom lagi jika diperlukan
         ]);
+
+        foreach ($penduduk as $row) {
+            fputcsv($handle, [
+                $row->nik,
+                $row->nkk,
+                $row->no_rt,
+                $row->nama,
+                $row->tempat_lahir,
+                $row->tanggal_lahir,
+                $row->alamat,
+                $row->jenis_kelamin->getDescription(),
+                $row->agama->getDescription(),
+                $row->pendidikan->getDescription(),
+                $row->pekerjaan->getDescription(),
+                $row->golongan_darah->getDescription(),
+                $row->status_pernikahan->getDescription(),
+                $row->status_pendatang
+                // Tambahkan kolom lagi jika diperlukan
+            ]);
+        }
+
+        fclose($handle);
+
+        $headers = [
+            'Content-Type' => 'text/csv',
+        ];
+
+        return Response::download($filename, 'penduduk.csv', $headers);
     }
-
-    fclose($handle);
-
-    $headers = [
-        'Content-Type' => 'text/csv',
-    ];
-
-    return Response::download($filename, 'penduduk.csv', $headers);
 }
-}
+
+
+
+
+
+    // //impor csv
+    // public function import(Request $request)
+    // {
+    //     $file = $request->file('file');
+
+    //     if (!$file) {
+    //         Alert::error('Error', 'File tidak ditemukan.');
+    //         return redirect()->back();
+    //     }
+
+    //     $fileContents = file($file->getPathname());
+
+    //     foreach ($fileContents as $key => $line) {
+    //         try {
+    //             $data = str_getcsv($line);
+
+    //             if (count($data) !== 14) {
+    //                 throw new \Exception('CSV tidak valid. Setiap baris harus memiliki 14 kolom.');
+    //             }
+
+    //             // Validasi input per baris
+    //             $validator = Validator::make([
+    //                 'nik' => $data[0],
+    //                 'nkk' => $data[1],
+    //                 'no_rt' => $data[2],
+    //                 'nama' => $data[3],
+    //                 'tempat_lahir' => $data[4],
+    //                 'tanggal_lahir' => $data[5],
+    //                 'alamat' => $data[6],
+    //                 'jenis_kelamin' => $data[7],
+    //                 'agama' => $data[8],
+    //                 'pendidikan' => $data[9],
+    //                 'pekerjaan' => $data[10],
+    //                 'golongan_darah' => $data[11],
+    //                 'status_pernikahan' => $data[12],
+    //                 'status_pendatang' => $data[13]
+    //             ], [
+    //                 'nik' => 'required|string|min:15|max:17|unique:penduduk,nik',
+    //                 'nkk' => 'required|string|min:15|max:17',
+    //                 'no_rt' => 'required|string|max:2',
+    //                 'nama' => 'required|string|max:49',
+    //                 'tempat_lahir' => 'required|string|min:2|max:49',
+    //                 'tanggal_lahir' => 'required|date',
+    //                 'alamat' => 'required|string|min:5',
+    //                 'jenis_kelamin' => [Rule::enum(JenisKelamin::class)],
+    //                 'agama' => [Rule::enum(Agama::class)],
+    //                 'pendidikan' => [Rule::enum(Pendidikan::class)],
+    //                 'pekerjaan' => [Rule::enum(Pekerjaan::class)],
+    //                 // 'golongan_darah' => 'required|string|max:2',
+    //                 'golongan_darah' =>  [Rule::enum(GolDar::class)],
+    //                 'status_pernikahan' => [Rule::enum(StatusPernikahan::class)],
+    //                 'status_pendatang' => 'required',
+    //             ]);
+
+    //             $validator->validate();
+
+    //             // Jika validasi berhasil, buat entri baru di database
+    //             Penduduk::create($validator->validated());
+    //         } catch (\Exception $e) {
+    //             $errors = $validator->errors()->all();
+    //             $errorMessage = 'Kesalahan pada baris ' . ($key + 1) . ': ' . implode(', ', $errors);
+    //             Alert::error('Error', $errorMessage);
+    //             return redirect()->back();
+    //         }
+    //     }
+
+    //     return redirect()->back()->with('success', 'CSV berhasil diimpor.');
+    // }
