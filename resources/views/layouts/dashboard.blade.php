96787b8c6a5435fc6a0fd4d0b700d9dec339e08a
avicennamumtaza <avislamongan@gmail.com>
2024-05-31 06:13:19 +0700

    fix: dashboard RW penduduk based on usia linked card and pengajuan dokumen migration

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,13 +5,16 @@
         @can('isRt')
             <section>
                 @if (auth()->user()->updated_at == auth()->user()->created_at)
-                <div class="alert alert-danger" role="alert">
-                    <h4 class="alert-heading">Perhatian!</h4>
-                    <p>Demi keamanan akun anda, harap perbarui password dengan segera. Password yang saat ini anda gunakan merupakan password ketika akun pertama kali dibuat dan perlu diperbarui. Pastikan untuk menggunakan password baru yang kuat dan unik untuk menjaga akun anda tetap aman.</p>
-                    <hr>
-                    <p class="mb-0">Anda bisa klik <a style="text-decoration: underline" href="{{ route('profil.manage') }}">link ini</a> untuk menggunakan fitur ubah password.</p>
-                </div>
-            @endif            
+                    <div class="alert alert-danger" role="alert">
+                        <h4 class="alert-heading">Perhatian!</h4>
+                        <p>Demi keamanan akun anda, harap perbarui password dengan segera. Password yang saat ini anda gunakan
+                            merupakan password ketika akun pertama kali dibuat dan perlu diperbarui. Pastikan untuk menggunakan
+                            password baru yang kuat dan unik untuk menjaga akun anda tetap aman.</p>
+                        <hr>
+                        <p class="mb-0">Anda bisa klik <a style="text-decoration: underline"
+                                href="{{ route('profil.manage') }}">link ini</a> untuk menggunakan fitur ubah password.</p>
+                    </div>
+                @endif
                 <div class="row">
                     <div class="col-12 mt-3 mb-3">
                         <h2 class="">Dashboard RT</h2>
@@ -215,9 +218,6 @@
 
 
 
-
-
-
         @can('isRw')
             <section>
                 <div class="row">
@@ -325,9 +325,6 @@
                     </div>
                 </div>
 
-
-
-
                 <div class="row">
                     <div class="col-12 mt-3 mb-3">
                         <h4>Statistik Pendataan Penduduk &amp; Klasifikasi Berdasarkan Usia</h4>
@@ -412,53 +409,68 @@
                 <div class="row">
                     <div class="col-xl-4 col-md-4 col-sm-12 mb-4">
                         <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between p-md-1">
-                                    <div class="d-flex flex-row">
-                                        <div class="align-self-center">
-                                            <h2 class="h3 mb-0 me-4">{{ $jumlahAnakAnak }}</h2>
+                            <a href="{{ route('anak') }}" style="text-decoration: none">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between p-md-1">
+                                        <div class="d-flex flex-row">
+                                            <div class="align-self-center">
+                                                <h2 class="h3 mb-0 me-4">{{ $jumlahAnakAnak }}</h2>
+                                            </div>
+                                            <div>
+                                                <h4 style="margin-bottom: 3px;">Anak-anak</h4>
+                                                <p class="mb-0">Jumlah Penduduk Dengan Usia < 14 tahun</p>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <h4 style="margin-bottom: 3px;">Anak-anak</h4>
-                                            <p class="mb-0">Jumlah Penduduk Dengan Usia < 14 tahun</p>
-                                        </div>
+                                        {{-- <button type="button" class="mt-2 btn primary position-relative">
+                                            >
+                                        </button> --}}
                                     </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
                     </div>
                     <div class="col-xl-4 col-md-4 col-sm-12 mb-4">
                         <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between p-md-1">
-                                    <div class="d-flex flex-row">
-                                        <div class="align-self-center">
-                                            <h2 class="h3 mb-0 me-4">{{ $jumlahUsiaProduktif }}</h2>
+                            <a href="{{ route('produktif') }}" style="text-decoration: none">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between p-md-1">
+                                        <div class="d-flex flex-row">
+                                            <div class="align-self-center">
+                                                <h2 class="h3 mb-0 me-4">{{ $jumlahUsiaProduktif }}</h2>
+                                            </div>
+                                            <div>
+                                                <h4 style="margin-bottom: 3px;">Usia Produktif</h4>
+                                                <p class="mb-0">Jumlah Penduduk Dengan Usia 15-64 tahun</p>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <h4 style="margin-bottom: 3px;">Usia Produktif</h4>
-                                            <p class="mb-0">Jumlah Penduduk Dengan Usia 15-64 tahun</p>
-                                        </div>
+                                        {{-- <button type="button" class="mt-2 btn primary position-relative">
+                                            >
+                                        </button> --}}
                                     </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
                     </div>
                     <div class="col-xl-4 col-md-4 col-sm-12 mb-4">
                         <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between p-md-1">
-                                    <div class="d-flex flex-row">
-                                        <div class="align-self-center">
-                                            <h2 class="h3 mb-0 me-4">{{ $jumlahLansia }}</h2>
+                            <a href="{{ route('lansia') }}" style="text-decoration: none">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between p-md-1">
+                                        <div class="d-flex flex-row">
+                                            <div class="align-self-center">
+                                                <h2 class="h3 mb-0 me-4">{{ $jumlahLansia }}</h2>
+                                            </div>
+                                            <div>
+                                                <h4 style="margin-bottom: 3px;">Lanjut Usia</h4>
+                                                <p class="mb-0">Jumlah Penduduk Dengan Usia > 65 tahun</p>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <h4 style="margin-bottom: 3px;">Lanjut Usia</h4>
-                                            <p class="mb-0">Jumlah Penduduk Dengan Usia > 65 tahun</p>
-                                        </div>
+                                        {{-- <button type="button" class="mt-2 btn primary position-relative">
+                                        >
+                                    </button> --}}
                                     </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
                     </div>
                 </div>
