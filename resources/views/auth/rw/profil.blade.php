a50d3c021c421a73b77cac03282db8efb5c18777
Indra <radeonaru@gmail.com>
2024-05-31 23:27:28 +0700

    feat: customize title on Profil

--- resources/views/auth/rw/profil.blade.php
+++ resources/views/auth/rw/profil.blade.php
@@ -8,7 +8,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="editUserPasswordModalLabel">Edit User</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" title="Tutup"></button>
                 </div>
                 <div class="modal-body justify-content-start text-start">
                     <form id="editUserPasswordForm" action="{{ route('profil.update', $users->id_user) }}" method="POST">
@@ -51,8 +51,8 @@
                                 name="password_confirmation">
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
-                            <button type="submit" class="btn btn-success">Simpan Perubahan</button>
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" title="Batal edit profil">Batal</button>
+                            <button type="submit" class="btn btn-success" title="Edit profil">Simpan Perubahan</button>
                         </div>
                     </form>
                 </div>
@@ -66,7 +66,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Ganti Foto Profil</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" title="Tutup"></button>
                 </div>
                 <div class="modal-body justify-content-start text-start">
                     <form action="{{ route('profil.foto', $users->id_user) }}" method="POST" enctype="multipart/form-data">
@@ -83,12 +83,12 @@
                             <label class="custom-file-label mb-2" for="foto_profil">Foto Profil</label>
                             <input type="file" class="form-control" id="foto_profil" name="foto_profil" onchange="previewImage(event)">
                             <div class="d-flex justify-content-center pt-4">
-                                <img id="image_preview" src="{{ asset($users->foto_profil ? 'Foto Users/' . $users->foto_profil : 'Foto Users/default.jpg') }}" class="img-thumbnail" style="height: 250px; width: 200px;" alt="Foto Profil">
+                                <img id="image_preview" src="{{ asset($users->foto_profil ? 'Foto Users/' . $users->foto_profil : 'Foto Users/default.jpg') }}" class="img-thumbnail" style="height: 250px; width: 200px;" alt="Foto Profil" title="Foto profil">
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer justify-content-end">
-                    <button type="submit" class="btn btn-success">Perbarui</button>
+                    <button type="submit" class="btn btn-success" title="Perbarui foto profil">Perbarui</button>
                 </div>
                 </form>
             </div>
@@ -112,10 +112,10 @@
                                     //$gravatar_url = "https://www.gravatar.com/avatar/$hash?s=200&d=mp";
                                 @endphp
                                 <img src="{{ asset('Foto Users/' . $users->foto_profil) }}" class="img-thumbnail"
-                                    style="height: 250px; width: 200px;" alt="">
+                                    style="height: 250px; width: 200px;" alt="" title="Foto profil anda">
                                 <div class="pt-4 mx-4">
                                     <button type="button" data-bs-toggle="modal" data-bs-target="#editPicture"
-                                        class="edit btn btn-edit btn-sm">
+                                        class="edit btn btn-edit btn-sm" title="Ganti foto profil">
                                         Ganti Foto Profil
                                     </button>
                                 </div>
@@ -128,28 +128,28 @@
                                     <div class="form-group mb-3">
                                         <label for="nik" class="form-label text-start">NIK</label>
                                         <input type="text" class="form-control" id="nik" name="nik"
-                                            value="{{ $users->nik }}" required readonly>
+                                            value="{{ $users->nik }}" required readonly title="NIK anda">
                                     </div>
                                     <div class="form-group mb-3">
                                         <label for="username" class="form-label text-start">Nama Pengguna</label>
                                         <input type="text" class="form-control" id="username"
-                                            value="{{ $users->username }}" name="username" required readonly>
+                                            value="{{ $users->username }}" name="username" required readonly title="Nama pengguna anda">
                                     </div>
                                     <div class="form-group mb-3">
                                         <label for="role" class="form-label text-start">Peran</label>
                                         <input type="text" class="form-control" id="role"
-                                            value="{{ $users->role }}" name="role" required readonly>
+                                            value="{{ $users->role }}" name="role" required readonly title="Peran anda">
                                     </div>
                                     <div class="form-group mb-3">
                                         <label for="email" class="form-label text-start">Email</label>
                                         <input type="email" class="form-control" id="email"
-                                            value="{{ $users->email }}" name="email" required readonly>
+                                            value="{{ $users->email }}" name="email" required readonly title="Email anda">
                                     </div>
 
                                     <div class="form-group row pe-5 py-2">
                                         <div class="offset-lg-11 offset-md-5">
                                             <button type="button" data-bs-toggle="modal"
-                                                data-bs-target="#editUserPasswordModal" class="edit btn btn-edit btn-sm">
+                                                data-bs-target="#editUserPasswordModal" class="edit btn btn-edit btn-sm" title="Ubah profil">
                                                 Edit
                                             </button>
 
