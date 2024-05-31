2db3e2c6b5e40c48db2d995c017e3e178a3b2bd4
Ana Bellatus <57426532+anabellatus@users.noreply.github.com>
2024-05-31 21:55:50 +0700

    fix: using nama from query result

--- app/DataTables/RWDataTable.php
+++ app/DataTables/RWDataTable.php
@@ -39,7 +39,7 @@ class RWDataTable extends DataTable
             data-id="' . $row->no_rw . '"
             data-nik_rw="' . $row->nik_rw . '"
             data-wa_rw="' . $row->wa_rw . '"
-            data-nama_rw="' . $nama_rw . '"
+            data-nama_rw="' . $row->nama_rw . '"
             data-bs-toggle="modal" data-bs-target="#showRwModal" class="show-user show btn btn-show btn-sm">Tampil</button>';
 
             $action .= '
