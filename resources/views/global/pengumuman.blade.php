a4e1b8b87520583ecf22f6e0214d17124cc6c542
AfrizalDS <angelicacoustarika@gmail.com>
2024-06-02 11:40:37 +0700

    Fix : Fix error in landing pengumuman im pengumuman empty

--- resources/views/global/pengumuman.blade.php
+++ resources/views/global/pengumuman.blade.php
@@ -13,7 +13,8 @@
             hingga informasi urgent seperti perubahan kebijakan pemerintah.</p>
         @foreach ($pengumumans as $pengumuman)
             <article class="postcard light blue" title="Pengumuman">
-                <a class="postcard__img_link" href="#" data-toggle="modal" data-target="#fotoModal{{ $pengumuman->id }}" title="Foto Pengumuman">
+                <a class="postcard__img_link" href="#" data-toggle="modal" data-target="#fotoModal{{ $pengumuman->id }}"
+                    title="Foto Pengumuman">
                     <img class="postcard__img"
                         src="{{ $pengumuman->foto_pengumuman ? asset('Foto Pengumuman/' . $pengumuman->foto_pengumuman) : 'https://img.freepik.com/free-photo/stylish-asian-girl-making-announcement-megaphone-shouting-with-speakerphone-smiling-inviting-people-recruiting-standing-blue-background_1258-89437.jpg?w=900' }}"
                         alt="Foto Pengumuman{{ $pengumuman->id }}" data-id="{{ $pengumuman->id }}"
@@ -21,7 +22,7 @@
                 </a>
 
                 <div class="postcard__text t-dark">
-                    <h1 class="postcard__title blue"  title="Judul Pengumuman">{{ $pengumuman->judul }}</h1>
+                    <h1 class="postcard__title blue" title="Judul Pengumuman">{{ $pengumuman->judul }}</h1>
                     {{-- <div class="postcard__subtitle small">
                         <time datetime="2020-05-25 12:00:00">
                             <i class="fas fa-calendar-alt mr-2"></i>
@@ -29,11 +30,11 @@
                         </time>
                     </div> --}}
                     <div class="postcard__bar"></div>
-                    <div class="postcard__preview-txt"  title="Deskripsi Pengumuman">
+                    <div class="postcard__preview-txt" title="Deskripsi Pengumuman">
                         {{ $pengumuman->deskripsi }}
                     </div>
                     <ul class="postcard__tagbox">
-                        <li class="tag__item"  title="Tanggal Pengumuman"><i
+                        <li class="tag__item" title="Tanggal Pengumuman"><i
                                 class="fas fa-tag mr-2"></i>{{ Carbon::parse($pengumuman->tanggal)->translatedFormat('d F Y') }}
                         </li>
                     </ul>
@@ -42,32 +43,33 @@
         @endforeach
     </div>
     <!-- Modal untuk setiap UMKM -->
-    <div class="modal fade" id="fotoModal{{ $pengumuman->id }}" tabindex="-1" role="dialog"
-        aria-labelledby="fotoModalLabel{{ $pengumuman->id }}" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header d-flex justify-content-between">
-                    <h5 class="modal-title" id="fotoModalLabel{{ $pengumuman->id }}">{{ $pengumuman->judul }}
-                    </h5>
-                    <i class="bi bi-x-lg text-danger" data-dismiss="modal" aria-label="Close"
-                        style="font-size: 1.5rem;"></i>
-                </div>
-                <div class="modal-body">
-                    <img id="modalImg{{ $pengumuman->id }}"
-                        src="{{ $pengumuman->foto_pengumuman ? asset('Foto pengumuman/' . $pengumuman->foto_pengumuman) : 'https://img.freepik.com/free-photo/stylish-asian-girl-making-announcement-megaphone-shouting-with-speakerphone-smiling-inviting-people-recruiting-standing-blue-background_1258-89437.jpg?w=900' }}"
-                        class="img-fluid" alt="Foto pengumuman{{ $pengumuman->id }}"
-                        style="min-width: 100%; max-height: auto;">
+    @if (isset($pengumuman) && !empty($pengumuman))
+        <div class="modal fade" id="fotoModal{{ $pengumuman->id }}" tabindex="-1" role="dialog"
+            aria-labelledby="fotoModalLabel{{ $pengumuman->id }}" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header d-flex justify-content-between">
+                        <h5 class="modal-title" id="fotoModalLabel{{ $pengumuman->id }}">{{ $pengumuman->judul }}</h5>
+                        <i class="bi bi-x-lg text-danger" data-dismiss="modal" aria-label="Close"
+                            style="font-size: 1.5rem;"></i>
+                    </div>
+                    <div class="modal-body">
+                        <img id="modalImg{{ $pengumuman->id }}"
+                            src="{{ $pengumuman->foto_pengumuman ? asset('Foto pengumuman/' . $pengumuman->foto_pengumuman) : 'https://img.freepik.com/free-photo/stylish-asian-girl-making-announcement-megaphone-shouting-with-speakerphone-smiling-inviting-people-recruiting-standing-blue-background_1258-89437.jpg?w=900' }}"
+                            class="img-fluid" alt="Foto pengumuman{{ $pengumuman->id }}"
+                            style="min-width: 100%; max-height: auto;">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
 
 
 
-     <!-- Bootstrap JS -->
-     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script>
         // Mengatur modal yang akan ditampilkan ketika gambar pengumuman diklik
         $(document).ready(function() {
