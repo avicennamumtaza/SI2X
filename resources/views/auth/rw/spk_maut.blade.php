fc535bb61a57d98c487c7db868e0de0ac2d387a7
AfrizalDS <angelicacoustarika@gmail.com>
2024-06-04 20:51:55 +0700

    Fix : centering all modal in spk MAUT

--- resources/views/auth/rw/spk_maut.blade.php
+++ resources/views/auth/rw/spk_maut.blade.php
@@ -4,7 +4,7 @@
 
 {{-- Ekspor Data Bansos MAUT --}}
 <div class="modal fade" id="exportMAUT" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-md">
+    <div class="modal-dialog modal-dialog-centered modal-md">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Ekspor Data</h5>
