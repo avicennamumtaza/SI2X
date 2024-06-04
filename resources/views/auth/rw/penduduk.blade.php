f94c190a1cad9ac53b1842f2787732bcdd841ca3
AfrizalDS <angelicacoustarika@gmail.com>
2024-06-04 21:06:36 +0700

    fix : delete penduduk

--- resources/views/auth/rw/penduduk.blade.php
+++ resources/views/auth/rw/penduduk.blade.php
@@ -491,7 +491,7 @@
     {{-- {{-- Delete penduduk --}}
     <div class="modal fade" id="deletePendudukModal" tabindex="-1" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
-        <div class="modal-dialo modal-dialog-centered modal-md">
+        <div class="modal-dialog modal-dialog-centered modal-md">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Konfirmasi Penghapusan</h5>
