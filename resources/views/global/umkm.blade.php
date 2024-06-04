57421f1af4868e3b92ec212b00495564b9253b52
AfrizalDS <angelicacoustarika@gmail.com>
2024-06-04 21:07:48 +0700

    Fix : centering all modal in UMKM global

--- resources/views/global/umkm.blade.php
+++ resources/views/global/umkm.blade.php
@@ -4,7 +4,7 @@
     <div class="container container-umkm col-10">
         <!-- Modal -->
         <div class="modal fade" id="ajukanUmkmModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-md">
+            <div class="modal-dialog modal-dialog-centered modal-md">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Pengajuan UMKM</h5>
