45b704fe80108f86f02e27f66b1029c007c4b889
avicennamumtaza <avislamongan@gmail.com>
2024-06-05 11:45:57 +0700

    fix: vite config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -9,26 +9,19 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'SIRW') }}</title>
-    {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap"
         rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-AvFd_Vyv.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('build/assets/app--CxZbYsQ.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-D9bWMgn2.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('bootstrap-icons.min.css') }}">
-
-    {{-- icons --}}
-    {{-- <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css"> --}}
-    {{-- <link rel="stylesheet" href="{{ asset('bootstrap-icons.min.css') }}"> --}}
-    {{-- <link rel="stylesheet" href="node_modules/"> --}}
 
 </head>
 
