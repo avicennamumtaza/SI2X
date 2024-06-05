0ee84d69c717a7682c0e77d6d2fa6b9c9ca80dd8
avicennamumtaza <avislamongan@gmail.com>
2024-06-05 09:59:25 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: [
-//                 'resources/sass/app.scss',
-//                 'resources/js/app.js',
-//             ],
-//             // publicDirectory: "../",
-//             refresh: true,
-//         }),
-//     ],
-// });
-
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            // publicDirectory: "../",
             refresh: true,
-            buildDirectory: 'public/build',
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-    },
 });
 
 // vite.config.js
