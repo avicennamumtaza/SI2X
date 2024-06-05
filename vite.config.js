b62f7b1c1d65ce68a47da0b05f3c508aa036fd98
avicennamumtaza <avislamongan@gmail.com>
2024-06-05 09:42:35 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,33 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/sass/app.scss',
+//                 'resources/js/app.js',
+//             ],
+//             // publicDirectory: "../",
+//             refresh: true,
+//         }),
+//     ],
+// });
+
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            // publicDirectory: "../",
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'public/build',
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+    },
 });
