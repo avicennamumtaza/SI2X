38c0062dc4de3106b21b17497d0672bc58d42cd4
avicennamumtaza <avislamongan@gmail.com>
2024-06-05 09:27:45 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
-            publicDirectory: "../",
+            // publicDirectory: "../",
             refresh: true,
         }),
     ],
