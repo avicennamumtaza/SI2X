d84953cbb6ff5258b244cfcbf9736cbe60d0e6e1
AfrizalDS <angelicacoustarika@gmail.com>
2024-06-07 09:40:28 +0700

    Feat : add dataChartRT

---
+++ app/Charts/DataPendudukRTChart.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Charts;
+
+use ArielMejiaDev\LarapexCharts\LarapexChart;
+use App\Models\Penduduk;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache; 
+use App\Models\Rt;
+
+class DataPendudukRTChart
+{
+    protected $chart;
+
+    public function __construct(LarapexChart $chart)
+    {
+        $this->chart = $chart;
+    }
+
+    public function build(): \ArielMejiaDev\LarapexCharts\PieChart
+    { 
+        $rt = Rt::where('nik_rt', auth()->user()->nik)->first()->toArray();
+        $noRt = $rt['no_rt']; // Simpan nomor RT di variabel
+        
+        $jumlahAnakAnak = Cache::remember('jumlahAnakAnak', 600, function () use ($noRt) {
+        $date = \Carbon\Carbon::now()->subYears(15)->format('Y-m-d');
+        $penduduk = Penduduk::where('no_rt', $noRt)->where('tanggal_lahir', '>', $date)->paginate(25);
+        return $penduduk->total();
+    });
+    
+    $jumlahUsiaProduktif = Cache::remember('jumlahUsiaProduktif', 600, function () use ($noRt) {
+        $dateMin = \Carbon\Carbon::now()->subYears(65)->format('Y-m-d');
+        $dateMax = \Carbon\Carbon::now()->subYears(15)->format('Y-m-d');
+        $penduduk = Penduduk::where('no_rt', $noRt)->whereBetween('tanggal_lahir', [$dateMin, $dateMax])->paginate(25);
+        return $penduduk->total();
+    });
+    
+    $jumlahLansia = Cache::remember('jumlahLansia', 600, function () use ($noRt) {
+        $date = \Carbon\Carbon::now()->subYears(66)->format('Y-m-d');
+        $penduduk = Penduduk::where('no_rt', $noRt)->where('tanggal_lahir', '<=', $date)->paginate(25);
+        return $penduduk->total();
+    });
+        return $this->chart->pieChart()
+        ->setTitle('Distribusi Penduduk Rw 06 Jodipan Malang Berdasarkan Usia')
+            ->setWidth(600)   
+        ->setHeight(360)
+            ->setSubtitle('Season 2021.')
+            ->addData([$jumlahAnakAnak, $jumlahUsiaProduktif, $jumlahLansia])
+            ->setLabels(['Anak-Anak ' ,
+            'Usia Produktif ',
+            'Lansia ']);
+    }
+}
