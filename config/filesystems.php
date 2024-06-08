ef2c943f8080946793d521b88464955a19c8616a
avicennamumtaza <avislamongan@gmail.com>
2024-06-09 06:22:05 +0700

    fix: actually not fix, but i try to fix, in image operational feature

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,10 +43,18 @@ return [
         //     'visibility' => 'public',
         // ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL') . '/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        // ],
+
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'root' => public_path('/'),
+            'url' => env('APP_URL') . '/public',
             'visibility' => 'public',
             'throw' => false,
         ],
